68d3a2fa50f2e4d4689ec58abb2138d3c0fdfb69
Abhay Swamy V K <1ms23ec004@msrit.edu>
2026-04-03 23:19:27 +0530

    Add: minimum-difference-between-highest-and-lowest-of-k-scores - submission-0

---
+++ Data Structures & Algorithms/minimum-difference-between-highest-and-lowest-of-k-scores/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int res=100001;
+        for(int i=k-1;i<nums.size();i++) res = min(res,nums[i]-nums[i-k+1]);
+        return res;
+    }
+};
