fff653a834cbdb87deb105320240ce246417fcf6
Abhay Swamy V K <1ms23ec004@msrit.edu>
2026-04-04 23:05:42 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> arr(26,0);
+        int left=0,res=0,maxfreq=0;
+        for(int right=0;right<s.size();right++){
+            arr[s[right]-'A']++;
+            maxfreq = max(maxfreq,arr[s[right]-'A']);
+            while(((right-left+1)-maxfreq)>k) arr[s[left++]-'A']--;
+            res = max(res,right-left+1);
+        }
+        return res;
+    }
+};
