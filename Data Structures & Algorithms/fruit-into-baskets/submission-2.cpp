250e77d22414cde9061c9ca22e308aa9d656e4a8
Abhay Swamy V K <1ms23ec004@msrit.edu>
2026-04-05 10:53:01 +0530

    Add: fruit-into-baskets - submission-2

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> freq;
+        int left=0,right=0,res=0;
+        for(right=0;right<fruits.size();right++){
+            freq[fruits[right]]++;
+            while(freq.size()>2){
+                freq[fruits[left]]--;
+                if(freq[fruits[left]]==0) freq.erase(fruits[left]);
+                left++;
+            }
+            res = max(res,right-left+1);
+        }
+        return res;
+    }
+};
