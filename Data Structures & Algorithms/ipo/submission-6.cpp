5023694c23669f465149a4109b09fd91e0794984
Abhay Swamy V K <1ms23ec004@msrit.edu>
2026-04-13 21:25:49 +0530

    Add: ipo - submission-6

---
+++ Data Structures & Algorithms/ipo/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> mincaphp;
+        for(int i=0;i<profits.size();i++) mincaphp.push({capital[i],profits[i]});
+        priority_queue<int> maxprofhp;
+        int count = 0;
+        int ans = w;
+        while(count < k){
+            while(mincaphp.size()>0 && mincaphp.top().first <= ans){
+                maxprofhp.push(mincaphp.top().second);
+                mincaphp.pop();
+            }
+            if(maxprofhp.size()>0){
+                ans += maxprofhp.top();
+                maxprofhp.pop();
+                count++;
+            }
+            else break;
+        }
+        return ans;
+    }
+};
