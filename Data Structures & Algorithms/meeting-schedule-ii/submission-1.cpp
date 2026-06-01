e1d30da46b6b41b7727d6d81c5bf651d6d29e05b
Abhay Swamy V K <1ms23ec004@msrit.edu>
2026-06-01 17:43:40 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        int high = 0;
+        for(auto interval : intervals) high = max(high,interval.end);
+        vector<int> res(high+1,0);  
+        for(int i=0;i<intervals.size();i++){
+            res[intervals[i].start]++;
+            res[intervals[i].end]--;
+        }
+        int sum = 0;
+        int high2 = 0;
+        for(int i=0;i<res.size();i++){
+            sum += res[i];
+            res[i] = sum;
+            high2 = max(res[i],high2);
+        }
+        return high2;
+    }
+};
