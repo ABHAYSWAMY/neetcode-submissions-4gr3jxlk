dd63e76e43ad45575c4c51aa9844263c73140bc6
Abhay Swamy V K <1ms23ec004@msrit.edu>
2026-06-02 21:40:49 +0530

    Add: reverse-linked-list-ii - submission-3

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-3.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode dummy(0); //safety if head is reversed
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        ListNode* curr = head;
+        for(int i=1;i<left;i++){ //trying to find the node where it starts and the node before it called prev
+            prev = curr;
+            curr = curr->next;
+        }
+        ListNode* start = curr;
+        curr = head;
+        for(int i=1;i<right;i++) curr=curr->next;//finding the right node where prev shld point
+        prev->next = curr;
+        ListNode* end = curr->next; //keep the node after the end
+        ListNode* prev2 = end;  //point starting node to end bec thats how it shld be
+        while(start!=end){
+            ListNode* front = start->next;  //reverse the linked list, everything is in position
+            start->next = prev2;
+            prev2 = start;
+            start = front;
+        }
+        return dummy.next;  //return head
+    }
+};
