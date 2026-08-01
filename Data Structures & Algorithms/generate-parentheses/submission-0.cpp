54f898ea903b557373b468de23ebc8c20f2324f6
Abhay Swamy V K <1ms23ec004@msrit.edu>
2026-08-01 17:18:24 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void backtrack(int open,int close,int n,string& curr,vector<string>& ans){
+        if(curr.size()==2*n){
+            ans.push_back(curr);
+            return;
+        }
+        if(open<n){
+            curr.push_back('(');
+            backtrack(open+1,close,n,curr,ans);
+            curr.pop_back();
+        }
+        if(close<open){
+            curr.push_back(')');
+            backtrack(open,close+1,n,curr,ans);
+            curr.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string curr;
+        vector<string> ans;
+        backtrack(0,0,n,curr,ans);
+        return ans;
+    }
+};
